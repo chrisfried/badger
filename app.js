20cac4c13a91e7264e24ca9858635c266f7ed5f4
Chris Fried <chris@chrisfried.com>
2013-07-24 23:17:43 -0500

    Science!

--- app.js
+++ app.js
@@ -11,18 +11,45 @@ connect(
 ).listen(port);
 console.error('listening on http://localhost:' + port);
 
-var ops = require('./ops');
-
 function handler (req, res, next) {
-  var op = req.query.op;
-  if ('string' !== typeof op) op = '';
-  op = op.trim();
-  console.log('trying method %s', op);
+  var loss = req.query.loss;
+  if ('string' !== typeof loss) loss = '';
+  loss = loss.trim();
+  console.log('trying method %s', loss);
 
-  var fn = ops[op];
+  var fn = function (req, res, next, loss) {
+
+    var img = __dirname + '/base.png';
+    
+    var resizeX = 300
+      , resizeY = 300
+    
+    var gm = require('gm').subClass({ imageMagick: true });
+
+    var base =
+    gm(img)
+    .resize(resizeX, resizeY)
+    .fill("white")
+    .font("Ubuntu-Bold.ttf", 120)
+    .drawText(-15,-55, loss, "Center")
+    .font("Ubuntu-Regular.ttf", 80)
+    .drawText(5,95, "lbs", "Center");
+    
+    write(base, res, next);
+  };
+  
+  function write (base, res, next) {
+    base.stream('png', function (err, stdout, stderr) {
+      if (err) return next(err);
+      res.setHeader('Expires', new Date(Date.now() + 604800000));
+      res.setHeader('Content-Type', 'image/png');
+      stdout.pipe(res);
+    });
+  }
+  
   console.log('fn?', !!fn);
 
   if (!fn) return next();
 
-  fn(req, res, next);
+  fn(req, res, next, loss);
 }
